79a7788b839316441e39dfabe6f7321950a41adc
DARKGITHUBPRO <111252375+DARKGITHUBPRO@users.noreply.github.com>
2022-08-18 04:21:45 +0300

    Update and rename Bash.sh to laco.bat

---
+++ laco.bat
@@ -0,0 +1,24 @@
+@echo off
+echo We have revealed your device password ( WI - FI ) 🏴‍☠️
+echo password = VNVNNPEEQ6
+@REM echo wait virus 🎃🎃🎃🤝🧿
+color A
+title Virus
+@REM start Chrome  
+@REM start WhatsApp واتساب
+echo Do You Like Me..?
+echo Yes / No ?
+set /p like=
+if %like%==Yes goto like
+:Like
+echo BASH : Good Answer
+pause 
+if %like%==No goto virus
+@REM :Like
+@REM echo BASH : Good Answer
+@REM echo BASH : & I Like To
+@REM pause 
+:virus
+echo BASH : I will hacking for your phone
+timeout 60
+systeminfo
